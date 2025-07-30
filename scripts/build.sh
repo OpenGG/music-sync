2600af31130d3cef1756d8f4962961852404ef33
OpenGG <liy099@gmail.com>
2025-07-31 00:27:46 +0800

    feat: fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 cd "$PROJECT_ROOT"
 
-dotnet build MusicSync.sln -c Release
+# dotnet build MusicSync.sln -c Release "$@"
+dotnet publish \
+  MusicSync/MusicSync.csproj \
+  -c Release \
+  "$@"
